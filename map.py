69919f0ba95d7e5537de99223eca43671d060e1c
Kaitlyn Ng <63861700+kaitlynng9@users.noreply.github.com>
2024-03-05 14:39:36 -0800

    tooltip functionality

--- map.py
+++ map.py
@@ -2,7 +2,6 @@ import branca
 import folium
 import geopandas as gpd
 import pandas as pd
-import requests
 import streamlit as st
 from folium.features import GeoJsonPopup, GeoJsonTooltip
 
@@ -17,26 +16,28 @@ def get_data():
 
 census_gdf, stop_gdf = get_data()
 
-# colormap = branca.colormap.LinearColormap(
-#     vmin=df["change"].quantile(0.0),
-#     vmax=df["change"].quantile(1),
-#     colors=["red", "orange", "lightblue", "green", "darkgreen"],
-#     caption="State Level Median County Household Income (%)",
-# )
+demographic_var = "PctBlack"
+
+colormap = branca.colormap.LinearColormap(
+    vmin=census_gdf[demographic_var].quantile(0.0),
+    vmax=census_gdf[demographic_var].quantile(1),
+    colors=["white", "red"],
+    caption=demographic_var
+)
 
 m = folium.Map(location=[47.751, -120.740], zoom_start=10)
 
 popup = GeoJsonPopup(
-    fields=["TractID", "PctBlack"],
-    aliases=["Tract", "% Black"],
+    fields=["TractID", demographic_var],
+    aliases=["Tract", demographic_var],
     localize=True,
     labels=True,
     style="background-color: yellow;",
 )
 
 tooltip = GeoJsonTooltip(
-    fields=["TractID", "PctBlack"],
-    aliases=["Tract", "% Black"],
+    fields=["TractID", demographic_var],
+    aliases=["Tract", demographic_var],
     localize=True,
     sticky=False,
     labels=True,
@@ -49,19 +50,22 @@ tooltip = GeoJsonTooltip(
     max_width=800,
 )
 
-folium.Choropleth(
-    geo_data=census_gdf,
-    name="Census Data",
-    data=census_gdf,
-    columns=["TractID", "PctBlack"],
-    key_on="feature.properties.TractID",
-    tooltip=tooltip,
-    popup=popup
+folium.GeoJson(
+    census_gdf,
+    style_function=lambda x: {
+        "fillColor": colormap(x["properties"][demographic_var])
+        if x["properties"][demographic_var] is not None
+        else "transparent",
+        "color": "black",
+        "fillOpacity": 0.4,
+    },
+    # tooltip=tooltip,
+    popup=popup,
 ).add_to(m)
 
 tooltip_stop = GeoJsonTooltip(
-    fields=["subject_race"],
-    aliases=["subject_race"],
+    fields=['subject_age', 'subject_race', 'subject_sex', 'citation_issued', 'warning_issued', 'outcome', 'contraband_found', 'frisk_performed', 'search_conducted'],
+    aliases=['Subject Age', 'Subject Race', 'Subject Sex', 'Citation Issued', 'Warning Issued','Outcome', 'Contraband Found', 'Frisk Performed', 'Search Conducted'],
     localize=True,
     sticky=False,
     labels=True,
@@ -75,11 +79,11 @@ tooltip_stop = GeoJsonTooltip(
 
 folium.GeoJson(
     stop_gdf.head(100),
-    columns=["TractID","subject_race"],
+    columns=["TractID",demographic_var],
     tooltip=tooltip_stop).add_to(m)
 
 
 
-# colormap.add_to(m)
+colormap.add_to(m)
 
 output = st_folium(m, width=700, height=500)
